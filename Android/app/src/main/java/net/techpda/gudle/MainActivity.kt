34aa5e03ad1c4015cb02ecd15a050f67821788cc
OLEI <zhwan85@gmail.com>
2018-12-07 17:49:08 +0900

    ㄹㄹㄹ
    ㄹㄹ

--- Android/app/src/main/java/net/techpda/gudle/MainActivity.kt
+++ Android/app/src/main/java/net/techpda/gudle/MainActivity.kt
@@ -1,18 +1,30 @@
 package net.techpda.gudle
 
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.ViewPager
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Display
 import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_one.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.viewPager
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.verticalLayout
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +46,8 @@ class MainActivity : AppCompatActivity() {
 //        val viewPager = findViewById<View>(R.id.viewpager) as ViewPager
 //        viewPager.adapter = CustomPagerAdapter(this, list)
 
+        // ListView Test
+        /*
         var hyoUrl: String = "http://heraldk.com/wp-content/uploads/2018/04/20180407000033_0.jpg"
 
 //        var d: Drawable = resources.getDrawable(R.drawable.p1)  drawable image call
@@ -54,7 +68,120 @@ class MainActivity : AppCompatActivity() {
             rv.addItemDecoration(DividerItemDecoration(applicationContext, LinearLayoutManager(this@MainActivity).orientation))
             rv.adapter = MovieAdapter(context, list)
         }
+        */
+
+        //WebView Test
+        val settings = webView.settings
+
+        settings.javaScriptEnabled = true
+
+        settings.setAppCacheEnabled(true)
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.setAppCachePath(cacheDir.path)
+
+        settings.setSupportZoom(true)
+        settings.builtInZoomControls = true
+        settings.displayZoomControls = true
+
+        settings.textZoom = 125
+
+        settings.blockNetworkImage = false
+
+        settings.loadsImagesAutomatically = true
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            settings.safeBrowsingEnabled = true
+        }
+
+        //settings.pluginState = WebSettings.PluginState.ON
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        settings.mediaPlaybackRequiresUserGesture = false
+
+
+        // More optional settings, you can enable it by yourself
+        settings.domStorageEnabled = true
+        settings.setSupportMultipleWindows(true)
+        settings.loadWithOverviewMode = true
+        settings.allowContentAccess = true
+        settings.setGeolocationEnabled(true)
+        settings.allowUniversalAccessFromFileURLs = true
+        settings.allowFileAccess = true
+
+        // WebView settings
+        webView.fitsSystemWindows = true
+
+
+        /*
+            if SDK version is greater of 19 then activate hardware acceleration
+            otherwise activate software acceleration
+        */
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
+
+        webView.webChromeClient = FullscreenableChromeClient(this)
+//        webView.webViewClient = object: WebViewClient() {
+//            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+//
+//
+//                toast("Page loading.")
+//
+//            }
+//
+//            override fun onPageFinished(view: WebView?, url: String?) {
+//                toast("Page Finished.")
+//            }
+//        }
+//
+//        webView.webChromeClient = object: WebChromeClient() {
+//            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+//                print("progress: $newProgress")
+//            }
+//        }
+
+
+        webView.loadUrl("https://m.post.naver.com/viewer/postView.nhn?volumeNo=16838844&memberNo=25041664&vType=VERTICAL")
+
     }
+
+    private fun showAppExitDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Please confirm")
+        builder.setMessage("No back history found, want to exit the app?")
+        builder.setCancelable(true)
+
+        builder.setPositiveButton("Yes", { _, _ ->
+            super@MainActivity.onBackPressed()
+        })
+
+        builder.setNegativeButton("No", { _, _ ->
+            toast("thank you.")
+        })
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+            toast("Going to back history")
+        } else {
+            showAppExitDialog()
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+
+        var d: Display = windowManager.defaultDisplay
+        var orientation = Configuration.ORIENTATION_UNDEFINED
+//        if(d.)
+    }
+}
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
 data class Movie(var title: String, var year: Int, var image: String, var color: Int)
