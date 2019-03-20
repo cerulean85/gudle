e14bf097849bafd023ec11823f5e5fe6ac207cd2
OLEI <zhwan85@gmail.com>
2019-03-20 17:43:33 +0900

    cdfddfb
    dfbdf

--- Android/app/src/main/java/net/techpda/gudle/MainView.kt
+++ Android/app/src/main/java/net/techpda/gudle/MainView.kt
@@ -6,33 +6,96 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.view_main.view.*
 import android.content.res.TypedArray
+import android.widget.ImageView
+import android.widget.TextView
+import org.jetbrains.anko.backgroundResource
+import org.jetbrains.anko.imageResource
 
 
+class MainView: LinearLayout{
 
-class MainView @JvmOverloads constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
-    init {
+    constructor(context: Context): super(context) { initView() }
+    constructor(context: Context, attrs: AttributeSet?): super(context, attrs) { initView(); getAttrs(attrs!!) }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr) { initView(); getAttrs(attrs!!) }
 
+    fun initView() {
 
-        val a = context.obtainStyledAttributes(attrs,
-                R.styleable.MainView, 0, 0)
-        val titleText = a.getString(R.styleable.MainView_titleText)
-        val valueColor = a.getColor(R.styleable.MainView_valueColor, resources.getColor(android.R.color.holo_blue_light))
-        a.recycle()
-
-
-//        LayoutInflater.from(context).inflate(R.layout.view_main, this, true)
-
-//        if (attrs != null) {
-//            val a = context.obtainStyledAttributes(attrs, R.styleable.custom_card_view)
-//            if (a.hasValue(R.styleable.custom_card_view_command)) {
-//                var myString = a.getString(R.styleable.custom_card_view_command)
-//            }
-//        }
-
-//        sendButton.setOnClickListener ({ nameLabel.text = "클릭: " + System.currentTimeMillis() })
+        val infService = Context.LAYOUT_INFLATER_SERVICE
+        val li = getContext().getSystemService(infService) as LayoutInflater
+        val v = li.inflate(R.layout.view_main, this, false)
+        addView(v)
     }
+
+    private fun getAttrs(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MainView)
+        setTypeArray(typedArray)
+    }
+
+    private fun setTypeArray(typedArray: TypedArray) {
+        setBG(typedArray.getResourceId(R.styleable.MainView_bg, R.drawable.p1))
+        setSymbol(typedArray.getResourceId(R.styleable.MainView_symbol, R.drawable.lego))
+        setTitle(typedArray.getString(R.styleable.MainView_text))
+        typedArray.recycle()
+    }
+
+    fun setBG(id: Int) {
+        baseView.backgroundResource = id
+    }
+
+    fun setSymbol(id: Int) {
+        symbolView.imageResource = id
+
+    }
+
+    fun setTitle(str: String) {
+        titleView.text = str
+    }
+
+
+
+
 }
+
+
+
+//
+//@JvmOverloads constructor(
+//        context: Context,
+//        attrs: AttributeSet? = null,
+//        defStyleAttr: Int = 0
+//) : LinearLayout(context, attrs, defStyleAttr) {
+//    init {
+//
+//
+//        val infService = Context.LAYOUT_INFLATER_SERVICE
+//        val li = getContext().getSystemService(infService) as LayoutInflater
+//        val v = li.inflate(R.layout.view_main, this, false)
+//        addView(v)
+//
+//
+//
+//
+//
+////        val a = context.obtainStyledAttributes(attrs,
+////                R.styleable.MainView, 0, 0)
+////        val titleText = a.getString(R.styleable.MainView_titleText)
+////        val valueColor = a.getColor(R.styleable.MainView_valueColor, resources.getColor(android.R.color.holo_blue_light))
+////        a.recycle()
+////
+////        setOnClickListener {
+////
+////
+////        }
+//
+////        LayoutInflater.from(context).inflate(R.layout.view_main, this, true)
+//
+////        if (attrs != null) {
+////            val a = context.obtainStyledAttributes(attrs, R.styleable.custom_card_view)
+////            if (a.hasValue(R.styleable.custom_card_view_command)) {
+////                var myString = a.getString(R.styleable.custom_card_view_command)
+////            }
+////        }
+//
+////        sendButton.setOnClickListener ({ nameLabel.text = "클릭: " + System.currentTimeMillis() })
+//    }
+//}
--- Android/app/src/main/java/net/techpda/gudle/fragments/TabMore.kt
+++ Android/app/src/main/java/net/techpda/gudle/fragments/TabMore.kt
@@ -5,13 +5,29 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_tab_more.view.*
 import net.techpda.gudle.R
+import net.techpda.gudle.Util
 
 class TabMore : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_more, container, false)
+
+        var v: View? = inflater.inflate(R.layout.fragment_tab_more, container, false)
+
+
+
+//        v!!.moreScv.layoutParams.height = Util.heightDisplay - Util.heightStatusBar - 400
+
+        v!!.testBtn.setOnClickListener {
+
+
+        }
+        v!!.requestLayout()
+
+
+        return v
     }
 }
